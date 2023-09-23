528c227e4b030f03fcdb4af10257297f6eac6e8d
Connor O'Brien <connorbobrien@gmail.com>
2023-09-23 11:43:12 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const firstName = "john";
+const lastName = "smith";
+
+const name = `${firstName} ${lastName}`;
+
+// const age = "44";
+const ageAsNumber = 38;
+
+const isOld = false;
+
+// console.log(typeof isOld);
+
+var number1 = "7";
+var number2 = "7";
+
+// strict comparison operator
+if (number1 === number2) {
+	console.log("they are equal");
+} else {
+	console.log("equal");
+}
+
+// const heading = document.querySelector("#heading");
+
+// heading.innerText = name;
+
+// console.log(heading);
+
+// // const
+
+// // let
