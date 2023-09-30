b9bad0e04927897b033685f7f35942a161bd5d06
Connor O'Brien <connorbobrien@gmail.com>
2023-09-30 11:20:34 -0300

    add first object

--- js/index.js
+++ js/index.js
@@ -1,35 +1,33 @@
-// let age = 7;
-// console.log(age);
-// age++;
-// console.log(age);
 const container = document.querySelector("#container");
 
-// for (let i = 1; i <= 10; i++) {
-// 	console.log(i);
-// 	const paragraph = document.createElement("p");
-// 	paragraph.innerText = i;
-// 	container.append(paragraph);
-// }
+const book = {
+	id: 123,
+	title: "Great book",
+	inStock: false,
+	genres: ["adventure", "sci-fi"],
+	publisher: {
+		name: "Some publisher",
+		address: "Somewhere",
+	},
+};
 
-const colours = ["red", "blue", "green", "yellow", "pink", "black"];
-console.log(colours);
-// var colour = colours[4];
-const arrayLength = colours.length;
-console.log("arrayLength", arrayLength);
+console.log(book.publisher.name);
 
-for (let i = 0; i < colours.length; i++) {
-	// console.log(i);
+const heading = document.createElement("h1");
+heading.innerText = book.title;
+container.append(heading);
 
-	console.log(i % 2);
+const genres = document.createElement("p");
+genres.innerText = book.genres;
+container.append(genres);
 
-	// console.log(colours[i]);
+const inStock = document.createElement("p");
+inStock.innerText = "In stock: ";
+const inStockValue = document.createElement("b");
+inStockValue.innerText = book.inStock;
+inStock.append(inStockValue);
+container.append(inStock);
 
-	const paragraph = document.createElement("p");
-	paragraph.innerText = colours[i];
-
-	if (i % 2 === 1) {
-		paragraph.classList.add("colour");
-	}
-
-	container.append(paragraph);
-}
+const publisherName = document.createElement("p");
+publisherName.innerText = book.publisher.name;
+container.append(publisherName);
