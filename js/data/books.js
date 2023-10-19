63d5dd7866d6432251bca689092d397be0556cf0
Connor O'Brien <connorbobrien@gmail.com>
2023-10-19 14:25:06 -0300

    modules

---
+++ js/data/books.js
@@ -0,0 +1,86 @@
+export const books = [
+	{
+		id: 123,
+		"book-title": "Great book",
+		inStock: false,
+		genres: ["adventure", "sci-fi"],
+		publisher: {
+			name: "Some publisher",
+			address: "Somewhere",
+		},
+	},
+	{
+		id: 124,
+		"book-title": "Another Great Book",
+		inStock: true,
+		genres: ["fantasy", "adventure"],
+		publisher: {
+			address: "Another Place",
+		},
+	},
+	{
+		id: 125,
+		"book-title": "Yet Another Great Book",
+		inStock: true,
+		genres: ["mystery"],
+	},
+	{
+		id: 126,
+		"book-title": "Fourth Great Book",
+		inStock: false,
+		genres: ["romance"],
+		publisher: {
+			address: "Fourth Place",
+		},
+	},
+	{
+		id: 127,
+		"book-title": "Fifth Great Book",
+		inStock: true,
+		genres: ["history"],
+		publisher: {
+			name: "Fifth Publisher",
+			address: "Fifth Place",
+		},
+	},
+	{
+		id: 128,
+		"book-title": "Sixth Great Book",
+		inStock: false,
+		genres: ["science"],
+		publisher: {
+			name: "Sixth Publisher",
+			address: "Sixth Place",
+		},
+	},
+	{
+		id: 129,
+		"book-title": "Seventh Great Book",
+		inStock: true,
+		genres: ["biography"],
+		publisher: {
+			name: "Seventh Publisher",
+			address: "Seventh Place",
+		},
+	},
+	{
+		id: 130,
+		"book-title": "Eighth Great Book",
+		inStock: false,
+		genres: ["philosophy"],
+		publisher: {
+			name: "Eighth Publisher",
+			address: "Eighth Place",
+		},
+	},
+	{
+		id: 131,
+		"book-title": "Ninth Great Book",
+		inStock: true,
+		genres: ["psychology"],
+		publisher: {
+			name: "Ninth Publisher",
+			address: "Ninth Place",
+		},
+	},
+];
